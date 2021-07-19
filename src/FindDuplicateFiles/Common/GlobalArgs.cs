a45d995ba222a1aee5c368cb59b6d2ca2fbe5e89
九零 <JiuLing-zhang@outlook.com>
2021-07-19 17:07:49 +0800

    测试新邮箱地址

--- src/FindDuplicateFiles/Common/GlobalArgs.cs
+++ src/FindDuplicateFiles/Common/GlobalArgs.cs
@@ -4,6 +4,9 @@ namespace FindDuplicateFiles.Common
 {
     public class GlobalArgs
     {
+        /// <summary>
+        /// 配置文件路径
+        /// </summary>
         public const string AppConfigPath = "/config.json";
         public static AppConfigInfo AppConfig = new();
     }

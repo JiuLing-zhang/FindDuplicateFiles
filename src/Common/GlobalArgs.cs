a05e362f64ae17cf5337faac19e61a7fc6abc2b8
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-02-19 18:10:16 +0800

    1、升级更新程序

--- src/Common/GlobalArgs.cs
+++ src/Common/GlobalArgs.cs
@@ -21,13 +21,6 @@ namespace FindDuplicateFiles.Common
         public static string AppConfigPath { get; set; } = Path.Combine(AppDataPath, AppName, "config.json");
 
         public static AppConfigInfo AppConfig { get; set; }
-        /// <summary>
-        /// 自动更新配置文件路径
-        /// </summary>
-        public const string UpdateConfigPath = "/Updates/Update";
-
-        public static UpdateConfigInfo UpdateConfig { get; set; } = new();
-
         public static string AppVersion { get; set; } = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
 
     }
